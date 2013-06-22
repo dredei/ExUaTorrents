20db240d60752b00696aa632cd11c0504dce4f03
dredei <xkadidas@gmail.com>
2013-06-22 18:09:43 +0300

    Вынес в отдельный файл.

---
+++ ExUa Torrents/ExUaFile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExUa_Torrents
+{
+    public class ExUaFile
+    {
+        public string name = string.Empty;
+        public string url = string.Empty;
+        public string torrentUrl = string.Empty;
+        public long size = 0;
+        public long fileId = -1;
+        public long arrIndex = -1;
+        public bool check = false;
+    }
+}
