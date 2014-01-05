e98b01dbe2aa7912f7f5766ea865f926d14fa102
dredei <xkadidas@gmail.com>
2014-01-05 17:50:16 +0200

    fix

--- C#/ExUa Torrents/Properties/AssemblyInfo.cs
+++ C#/ExUa Torrents/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion( "2.1.0.0" )]
-[assembly: AssemblyFileVersion( "2.1.0.0" )]
+[assembly: AssemblyFileVersion( "2.1.0.1" )]
